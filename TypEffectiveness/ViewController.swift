5b9d14cb181c2df51237d0717bbc20a359192615
Martin Plut <martinplutphoto@gmail.com>
2022-01-07 13:41:03 -0800

    Initial Commit

---
+++ TypEffectiveness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TypEffectiveness
+//
+//  Created by Martin Plut on 1/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
